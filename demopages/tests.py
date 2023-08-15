308a4cc597fad3c795b7a32e82928e41b8acc95c
Deniss Hohlov <deniss.hohlov@gmail.com>
2023-08-15 16:17:25 +0300

    repaired

--- demopages/tests.py
+++ demopages/tests.py
@@ -5,11 +5,7 @@ class IndexTestCase(TestCase):
     def setUp(self) -> None:
         self.client = Client()
 
-
     def test_get_index_endpoint(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.content, b'Hello World')
-
-# Create your tests here.
-
