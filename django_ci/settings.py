dde58479f9ba3e0137e6eb2222513bcafe96f864
Deniss Hohlov <deniss.hohlov@gmail.com>
2023-08-16 17:00:38 +0300

    settings.py updated

--- django_ci/settings.py
+++ django_ci/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'demopages.DemopagesConfig',
+    'demopages.apps.DemopagesConfig',
 ]
 
 MIDDLEWARE = [
